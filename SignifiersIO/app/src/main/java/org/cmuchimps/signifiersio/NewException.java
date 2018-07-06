2fb43c8e5118968bbdd73987848425d06366620e
Maxwell Johnson <maximusjohnsonus@gmail.com>
2018-07-05 20:46:28 -0400

    Created basic saving, loading, and editing of privacy policy

--- SignifiersIO/app/src/main/java/org/cmuchimps/signifiersio/NewException.java
+++ SignifiersIO/app/src/main/java/org/cmuchimps/signifiersio/NewException.java
@@ -1,7 +1,17 @@
 package org.cmuchimps.signifiersio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class NewException extends AppCompatActivity {
 
@@ -10,4 +20,44 @@ public class NewException extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_exception);
     }
+
+    // Create the JSON of the current exception and return it as a result
+    private void returnException(){
+        Intent intent = new Intent();
+        intent.putExtra(getResources().getString(R.string.exception_json), createException().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    // Represent the current fields as a JSONObject
+    private JSONObject createException(){
+        JSONObject exception = new JSONObject();
+
+        try{
+            exception.put("company",((EditText)findViewById(R.id.company_edit)).getText());
+        } catch(JSONException e){
+            Log.e("NewException", e.toString());
+        }
+
+        return exception;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.exception_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.done:
+                returnException();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
--- SignifiersIO/app/src/main/java/org/cmuchimps/signifiersio/SettingsActivity.java
+++ SignifiersIO/app/src/main/java/org/cmuchimps/signifiersio/SettingsActivity.java
@@ -3,23 +3,25 @@ package org.cmuchimps.signifiersio;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -34,36 +36,7 @@ import java.util.List;
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class SettingsActivity extends AppCompatPreferenceActivity {
-
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
-    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
-
-            if (preference instanceof ListPreference) {
-                // For list preferences, look up the correct display value in
-                // the preference's 'entries' list.
-                ListPreference listPreference = (ListPreference) preference;
-                int index = listPreference.findIndexOfValue(stringValue);
-
-                // Set the summary to reflect the new value.
-                preference.setSummary(
-                        index >= 0
-                                ? listPreference.getEntries()[index]
-                                : null);
-
-            } else {
-                // For all other preferences, set the summary to the value's
-                // simple string representation.
-                preference.setSummary(stringValue);
-            }
-            return true;
-        }
-    };
+    private static final int NEW_EXCEPTION_REQUEST = 1;
 
     /**
      * Helper method to determine if the device has an extra-large screen. For
@@ -151,21 +124,149 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class PolicyPreferenceFragment extends PreferenceFragment {
+        private boolean rootAllow;
+        private ArrayList<JSONObject> exceptions;
+
+        SharedPreferences sharedPreferences;
+        PreferenceCategory exceptionCategory;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_policy);
 
+            // Initialize global variables
+            exceptions = new ArrayList<>();
+            sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+            exceptionCategory = (PreferenceCategory)findPreference(getString(R.string.exception_category));
+
+            try{
+                // Load stored privacy policy from Shared Preferences
+                String privacyPolicyJSON = sharedPreferences.getString(getString(R.string.privacy_policy),
+                                                                       getString(R.string.default_policy));
+                JSONObject privacyPolicy = new JSONObject(privacyPolicyJSON);
+
+                // Parse the privacy policy to set the global variables
+                rootAllow = privacyPolicy.getString("rule_type").equals("allow");
+                if(privacyPolicy.has("except")){
+                    JSONArray es = privacyPolicy.getJSONArray("except");
+
+                    for(int i = 0; i < es.length(); i++){
+                        exceptions.add(es.getJSONObject(i));
+                    }
+                }
+
+            } catch(JSONException e){
+                Log.e("onCreate PolicyFragment",e.toString());
+                return; // TODO: maybe a toast or something more elegant, but this shouldn't happen
+            }
+
+            // Select the correct option in the list
+            updateAllowList();
+
+            // Create a Preference from each exception and add it to the view
+            for(int i = 0; i < exceptions.size(); i++){
+                // TODO: make the order stable by controlling order parameter
+                addExceptionPreference(exceptions.get(i),i);
+            }
+
+
+            // Disable saving the root allow type as a preference
+            final ListPreference listPreference = (ListPreference)findPreference(getString(R.string.root_allow_preference));
+            listPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object value) {
+                    int index = listPreference.findIndexOfValue(value.toString());
+
+                    rootAllow = (index == 0); // Update root allow
+                    updateAllowList();        // Update visuals
+                    updatePP();               // Save privacy policy
+
+                    // Do not save as preference
+                    return false;
+                }
+            });
+
+            // Add action to "Add Exception" button
             Preference button = findPreference(getString(R.string.exception_button));
             button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
+                    // Start an activity that handles making a new exception
                     Intent intent = new Intent(getActivity(), NewException.class);
-                    startActivity(intent);
+                    startActivityForResult(intent, NEW_EXCEPTION_REQUEST);
                     return true;
                 }
             });
         }
+
+        // Called when the new exception activity finishes
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // If the NewException activity returned a new exception
+            if (requestCode == NEW_EXCEPTION_REQUEST && resultCode == RESULT_OK) {
+                try {
+                    String exceptionJSON = data.getStringExtra(getString(R.string.exception_json));
+                    JSONObject exception = new JSONObject(exceptionJSON);
+
+                    // Add the new exception to our list
+                    int index = exceptions.size();
+                    exceptions.add(exception);
+
+                    // Create a Preference to display and allow editing of the exception
+                    addExceptionPreference(exception, index);
+
+                    // Save the new policy
+                    updatePP();
+                } catch(JSONException e) {
+                    Log.d("onActivityResult",e.toString());
+                }
+            }
+        }
+
+        // Set the root rule type based on rootAllow
+        private void updateAllowList(){
+            ListPreference listPreference = (ListPreference)findPreference(getString(R.string.root_allow_preference));
+            listPreference.setValueIndex(rootAllow ? 0 : 1);
+        }
+
+        private void addExceptionPreference(JSONObject exception, int order){
+            Preference p = new Preference(getContext());
+            p.setTitle(summarizeException(exception));
+            p.setOrder(order);
+            // TODO: set onclick to handle edits
+
+            // Add this Preference to the view
+            exceptionCategory.addPreference(p);
+        }
+
+        // Create a short and meaningful description of the exception
+        private String summarizeException(JSONObject exception) {
+            // TODO: do this
+            return exception.toString();
+        }
+
+        // Rebuild and update stored privacy policy
+        private void updatePP(){
+            try {
+                // Create the privacy policy JSON
+                JSONObject newPrivacyPolicy = new JSONObject();
+                newPrivacyPolicy.put("rule_type", rootAllow ? "allow" : "disallow");
+                newPrivacyPolicy.put("except",new JSONArray(exceptions));
+
+                // Save it to SharedPreferences
+                SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+                sharedPreferencesEditor.putString(getString(R.string.privacy_policy), newPrivacyPolicy.toString());
+                sharedPreferencesEditor.apply();
+                // TODO: propagate the new privacy policy
+            } catch(JSONException e){
+                Log.e("updatePP",e.toString());
+                // TODO: make a toast to let user know it didn't save, even though this should never happen
+            }
+        }
+
 
     }
 
